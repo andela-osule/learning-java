d92f1ad26fe612bf22f95ab674a6334242fcc679
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-03-31 15:48:46 +0100

    using variables

---
+++ src/UsingVariablesApp.java
@@ -0,0 +1,30 @@
+
+public class UsingVariablesApp {
+
+    public static void main(String[] args) {
+        // Using variables:. 8 primitive types
+    	int myNumber = 88; //32-bit value
+    	short myShort = 847; //16-bit value
+    	long myLong = 9797; //64-bit value
+    	
+    	double myDouble = 99.99; //double precision & floating point
+    	float myFloat = 99.9f; //float
+    	
+    	char myChar = 'C'; // single unicode character
+    	boolean myBoolean = false;
+    	
+    	byte myByte = 127; // 8-bits
+    	
+    	
+    	System.out.println(myNumber);
+    	System.out.println(myShort);
+    	System.out.println(myLong);
+    	System.out.println(myDouble);
+    	System.out.println(myFloat);
+    	System.out.println(myChar);
+    	System.out.println(myBoolean);
+    	System.out.println(myByte);
+    	
+    }
+
+}
