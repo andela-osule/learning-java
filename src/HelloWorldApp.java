88275abf7fc31c465afa1496788aa636b1851f1b
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-03-31 15:25:29 +0100

    A first in Java

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,15 @@
+/**
+ * @author oluwafemisule
+ *
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    	System.out.println("Hello world");
+    	
+    }
+
+}
