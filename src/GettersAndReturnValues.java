10c63a9e469a6f015a5bf6561e277f0eee7ec62f
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-11 08:47:26 +0100

    Getters and return values

---
+++ src/GettersAndReturnValues.java
@@ -0,0 +1,9 @@
+
+public class GettersAndReturnValues {
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.name = "P Q";
+		p.age = 25;
+		System.out.println(p.calculateYearsToRetirement());
+	}
+}
