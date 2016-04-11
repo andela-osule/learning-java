e4a0e3773987f1caeee9ce06f35666a90a21f3fb
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-11 08:47:11 +0100

    Methods

---
+++ src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+	public static void main(String[] args) {
+		Person jake  = new Person();
+		jake.name = "Jake Bloggs";
+		jake.age = 37;
+		
+		Person sarah = new Person();
+		sarah.name = "Sarah Smith";
+		sarah.age = 20;
+		
+		sarah.speak();
+	}
+}
