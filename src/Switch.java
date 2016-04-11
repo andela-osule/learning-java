921a2b65941bb6f0f39dbfc034922f0caec3144c
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-11 04:38:30 +0100

    Switches

---
+++ src/Switch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Switch {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter a command: ");
+
+		String text = scanner.nextLine();
+
+		switch (text) {
+		case "start":
+			System.out.println("Machine started!");
+			break;
+		case "stop":
+			System.out.println("Machine stopped.");
+			break;
+		default:
+			System.out.println("Command not recognized.");
+		}
+		scanner.close();
+	}
+
+}
