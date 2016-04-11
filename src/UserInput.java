f59bdd730031a9d7c065c63477300d19c60be6f2
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-11 04:37:46 +0100

    User input

---
+++ src/UserInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UserInput {
+	public static void main(String[] args) {
+		// Create scanner object
+		Scanner input = new Scanner(System.in);
+
+		// Output the prompt
+		System.out.println("Enter ah line of text: ");
+
+		// Wait for user to enter line of text
+		String line = input.nextLine();
+		// double value = input.nextDouble();
+		// int value = input.nextInt();
+
+		// Print line of text
+		System.out.println("You entered: " + line);
+		input.close();
+	}
+}
