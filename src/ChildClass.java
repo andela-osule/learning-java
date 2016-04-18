7353d3a529a8d88c865f5e92bb237d237ee3ec34
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-04-18 11:53:44 +0100

    Packages

---
+++ src/ChildClass.java
@@ -0,0 +1,14 @@
+
+public class ChildClass extends ParentClass {
+	public void wipeWindShield() {
+		System.out.println("Wiping windshield");
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Car started");
+	}
+
+
+	
+}
---
+++ src/UseFish.java
@@ -0,0 +1,11 @@
+import ocean.Fish;
+import ocean.plants.Seaweed;
+
+public class UseFish {
+	public static void main(String[] args) {
+		Fish fish = new Fish();
+		Seaweed weed = new Seaweed();
+	}
+} // You can have packages within packages.
+// Name packages with the reverse name of your website e.g
+// com.yahoo.<packagename>
