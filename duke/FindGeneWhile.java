8160ff26d45283442f063d81df1139d15df51831
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-11-14 00:46:25 +0100

    search gene until stop codon found

---
+++ duke/FindGeneWhile.java
@@ -0,0 +1,17 @@
+public class FindGeneWhile {
+    public String findGene(String dna) {
+        int startIndex = dna.indexOf("ATG");
+        int currIndex = dna.indexOf("TAA", startIndex + 3);
+        
+        while (currIndex != -1) {
+            if((currIndex - startIndex) % 3 == 0) {
+                return dna.substring(startIndex, currIndex+3);
+            }
+            else {
+                currIndex = dna.indexOf("TAA", currIndex + 1);
+            
+            }
+        }
+        return "";
+    }
+}
