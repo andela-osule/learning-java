c2e4b8d44c9cf833b10628213d8242b99220d794
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-11-23 12:13:23 +0100

    find max temperature in day records

---
+++ mooc/CSVMaxTemp.java
@@ -0,0 +1,32 @@
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+
+
+public class CSVMaxTemp {
+
+    public CSVRecord hottestHourInFile(CSVParser parser) {
+        CSVRecord largestSoFar = null;
+
+        for(CSVRecord currentRow : parser) {
+            if(largestSoFar == null) {
+                largestSoFar = currentRow;
+            } else {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                if(currentTemp > largestTemp) {
+                    largestSoFar = currentRow;
+                }
+            }
+        }
+
+        return largestSoFar;
+    }
+
+    public void testHottestInDay() {
+        FileResource fr = new FileResource("data/2015/weather-2015-01-01.csv");
+        CSVRecord largest = hottestHourInFile(fr.getCSVParser());
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+    }
+
+}
