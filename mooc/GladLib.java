87ce84bc77a92f6a7f3bd7062483325571e0b9e8
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-21 15:15:45 +0100

    refactor gladlib to use hashmap

--- mooc/GladLib.java
+++ mooc/GladLib.java
@@ -2,17 +2,10 @@ import edu.duke.*;
 import java.util.*;
 
 public class GladLib {
-    private ArrayList<String> adjectiveList;
-    private ArrayList<String> nounList;
-    private ArrayList<String> colorList;
-    private ArrayList<String> countryList;
-    private ArrayList<String> nameList;
-    private ArrayList<String> animalList;
-    private ArrayList<String> timeList;
-    private ArrayList<String> verbList;
-    private ArrayList<String> fruitList;
-    
+    private HashMap<String, ArrayList<String>> myMap;
+
     private ArrayList<String> seenList =  new ArrayList<String>();
+    private ArrayList<String> categories = new ArrayList<String>();
     
     private Random myRandom;
 
@@ -22,20 +15,21 @@ public class GladLib {
 
 
     public GladLib(){
+        myMap = new HashMap<String, ArrayList<String>>();
         initializeFromSource(dataSourceDirectory);
         myRandom = new Random();
     }
 
     private void initializeFromSource(String source) {
-        adjectiveList = readIt(source + "/adjective.txt");
-        nounList = readIt(source + "/noun.txt");
-        colorList = readIt(source + "/color.txt");
-        countryList = readIt(source + "/country.txt");
-        nameList = readIt(source + "/name.txt");
-        animalList = readIt(source + "/animal.txt");
-        timeList = readIt(source + "/timeframe.txt");
-        verbList = readIt(source + "/verb.txt");
-        fruitList = readIt(source + "/fruit.txt");
+        String[] categories = {
+            "adjective", "noun", "color", "country", 
+            "name", "animal", "time", "verb", "fruit"};   
+        for(String category : categories) {
+            if(category.equals("time")) {
+                category = "timeframe";
+            }
+            myMap.put(category, readIt(source + "/" + category + ".txt"));
+        }
     }
 
     private String randomFrom(ArrayList<String> source) {
@@ -44,35 +38,15 @@ public class GladLib {
     }
 
     private String getSubstitute(String label) {
-        if(label.equals("adjective")){
-            return randomFrom(adjectiveList);
-        }
-        if(label.equals("noun")){
-            return randomFrom(nounList);
-        }
-        if(label.equals("color")){
-            return randomFrom(colorList);
-        }
-        if(label.equals("country")){
-            return randomFrom(countryList);
-        }
-        if(label.equals("name")){
-            return randomFrom(nameList);
-        }
         if(label.equals("number")){
             return "" + myRandom.nextInt(50)+5;
         }
-        if(label.equals("animal")){
-            return randomFrom(animalList);
-        }
-        if(label.equals("timeframe")){
-            return randomFrom(timeList);
-        }
-        if(label.equals("verb")){
-            return randomFrom(verbList);
-        }
-        if(label.equals("fruit")){
-            return randomFrom(fruitList);
+
+        if(myMap.containsKey(label)) {
+            if(!categories.contains(label)) {
+                categories.add(label);
+            }
+            return randomFrom(myMap.get(label));
         }
         return "**UNKNOWN**";
     }
@@ -148,5 +122,25 @@ public class GladLib {
 		String story = fromTemplate("data/madtemplate2.txt");
 		printOut(story, 60);
 		System.out.println("\nNumber of replaced words: " + numReplacedWords);
+		
+		System.out.println("Total words in map: " + totalWordsInMap());
+		System.out.println("Total words considered: " + totalWordsConsidered());
+	}
+	
+	public int totalWordsInMap() {
+	    int total = 0;
+	    
+	    for(ArrayList<String> wordsList: myMap.values()) {
+	        total += wordsList.size();
+	    }
+	    return total;
+	}
+	
+	public int totalWordsConsidered() {
+	    int total = 0;
+	    for(String category : categories) {
+	        total += myMap.get(category).size();
+	    }
+	    return total;
 	}
 }
