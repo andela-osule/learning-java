0b5d463b896ee1ea55475c5b105d4b73f560b3f3
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-12-23 18:00:25 +0100

    count visits per IP

--- mooc/LogAnalyzer.java
+++ mooc/LogAnalyzer.java
@@ -36,4 +36,62 @@ public class LogAnalyzer {
     
         return uniqueIPs.size();
     }
+
+    public void printAllHigherThanNum(int num) {
+        for(LogEntry le: records) {
+            int statusCode = le.getStatusCode();
+            
+            if(statusCode > num) {
+                System.out.println(le);
+            }
+             
+        }
+    }
+    
+    public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+        ArrayList<String> uniqueIPVisits = new ArrayList<String>();
+        
+        for(LogEntry le: records) {
+            Date accessTime = le.getAccessTime();
+            String accessDay = accessTime.toString();
+
+            String IPAddress = le.getIPAddress();
+            
+            if(accessDay.indexOf(someday) != -1 && !uniqueIPVisits.contains(IPAddress)) {
+                uniqueIPVisits.add(IPAddress);
+            }
+        }
+        
+        return uniqueIPVisits;
+    }
+    
+    public int countUniqueIPsInRange(int low, int high) {
+        ArrayList<String>  uniqueIPs = new ArrayList<String>();
+        for(LogEntry le: records) {
+            int statusCode = le.getStatusCode();
+            String IPAddress = le.getIPAddress();
+            
+            if(statusCode >= low  && statusCode <= high && !uniqueIPs.contains(IPAddress)) {
+                uniqueIPs.add(IPAddress);
+            }           
+        }
+        return uniqueIPs.size();
+    }
+    
+    public HashMap<String, Integer> countVisitsPerIP() {
+        HashMap<String, Integer> counts = new HashMap<String, Integer>();
+        
+        for(LogEntry le: records) {
+            String IP = le.getIPAddress();
+            
+            if(counts.containsKey(IP)) {
+                counts.put(IP, counts.get(IP) + 1);
+            } else {
+                counts.put(IP, 1);
+            }
+        
+        }
+        
+        return counts;
+    }
 }
--- mooc/LogTester.java
+++ mooc/LogTester.java
@@ -16,4 +16,48 @@ public class LogTester {
         la.printAll();
     }
 
+    public void testUniqueIP() {
+        LogAnalyzer la = new LogAnalyzer();
+
+        la.readFile("data/short-test.log");
+        la.countUniqueIPs();
+    }
+
+    public void testPrintAllHigherThanNum() {
+        LogAnalyzer la = new LogAnalyzer();
+
+        la.readFile("data/weblog1_log.txt");
+        la.printAllHigherThanNum(400);
+    }
+    
+    public void  testUniqueIPVisitsOnDay() {
+        LogAnalyzer la = new LogAnalyzer();
+        
+        la.readFile("data/weblog1_log.txt");
+        ArrayList<String> uniqueIPVisits = la.uniqueIPVisitsOnDay("Mar 17");
+        System.out.println("Unique IP Visits: " + uniqueIPVisits.size());
+        
+        for(String uniqueIP: uniqueIPVisits) {
+            System.out.println(uniqueIP);
+        }
+    }
+    
+    public void testCountUniqueIPsInRange() {
+        LogAnalyzer la = new LogAnalyzer();
+        
+        la.readFile("data/weblog1_log.txt");
+        int count = la.countUniqueIPsInRange(200, 299);
+        System.out.println("UniqueIPs in range: " + count);
+    }
+    
+    public void testCounts() {
+        LogAnalyzer la = new LogAnalyzer();
+        
+        la.readFile("data/short-test_log.txt");
+        HashMap<String, Integer> counts = la.countVisitsPerIP();
+        
+        System.out.println(counts);
+        
+        
+    }
 }
